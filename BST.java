c05edf680f01c07240950f0b4efb51d94ecd048e
Oscar Rompich <oscarligo6@gmail.com>
2025-03-27 19:14:22 -0600

    Estructura del trabajo, y clase nodo completa que se utilzará

---
+++ BST.java
@@ -0,0 +1,13 @@
+/** 
+ * Clase para un Binary Search Tree (BST) o arbol de busqueda binaria.
+ */
+
+public class BST <K extends Comparable<K>> {
+
+
+
+
+
+
+
+}
---
+++ Node.java
@@ -0,0 +1,56 @@
+public class Node <K, V> {
+
+    K key;
+    V value;
+    Node<K, V> left;
+    Node<K, V> right;
+
+
+    public Node(K key, V value) {
+        this.key = key;
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+
+    public Node<K, V> getLeft() {
+        return left;
+    }
+
+
+    public Node<K, V> getRight() {
+        return right;
+    }
+
+
+    public void setLeft(Node<K, V> left) {
+        this.left = left;
+    }
+
+    public void setRight(Node<K, V> right) {
+        this.right = right;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    
+
+
+
+    
+}
